b535e4ef175f2926af4ede5cc3ca1fdcbf3278aa
Jeff <jeff.tang@live.com>
2018-05-14 13:04:38 -0400

    finished step 5

---
+++ step5/step5.js
@@ -0,0 +1,31 @@
+var https = require('https');
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+
+function printHTML(html) {
+  console.log(html);
+}
+
+// getHTML(requestOptions, printHTML)
+
+module.exports = function getHTML(options, callback) {
+  let dataRec = '';
+
+  /* Add your code here */
+
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function (chunk) {
+      dataRec += chunk;
+    })
+
+    response.on('end', function (data) {
+      callback(dataRec)
+    })
+
+  })
+};
